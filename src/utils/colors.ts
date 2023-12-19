011c8824cc844169c4a5903fa94715f572148561
MrNwear <mr.y.neuer@gmail.com>
2023-12-20 01:39:34 +0200

    colors pallete added

---
+++ src/utils/colors.ts
@@ -0,0 +1,14 @@
+type ColorsType = {
+  Primary: string;
+  black: string;
+  white: string;
+  yellow: string;
+};
+
+const COLORS: ColorsType = {
+  Primary: "#072040",
+  black: "#000",
+  white: "#fff",
+  yellow: "#FFC709",
+};
+export default COLORS;
